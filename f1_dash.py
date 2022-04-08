6f6ce0b6485a583f07c07a5116ebba975777461b
diogobulhosa <dibulhosa@gmail.com>
2022-04-08 12:41:31 +0100

    fastest lap available + circuit on click

--- f1_dash.py
+++ f1_dash.py
@@ -5,12 +5,13 @@ from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
 # Dataset Processing
 
 path = 'https://raw.githubusercontent.com/nalpalhao/DV_Practival/master/datasets/Lesson_1/'
 
-df_final = pd.read_excel('./data/circuits_drivers_races_constructors_results.xlsx')
+df_final = pd.read_excel('./data/cdrcr.xlsx')
 
 
 geo_path = 'https://raw.githubusercontent.com/nalpalhao/DV_Practival/master/datasets/Lesson_4/'
@@ -29,6 +30,7 @@ circuit_options = [
     for circuit in df_final['circuits.name'].unique()]
 
 # pedir oinião prof df_final['races.year'].max()
+#  value=[df_final['races.year'].min(), df_final['races.year'].max()]
 season_slider = dcc.RangeSlider(
         id='season_slider',
         min=df_final['races.year'].min(),
@@ -45,6 +47,14 @@ dropdown_circuits = dcc.Dropdown(
         value=['']
     )
 """
+
+def time_to_mili(s):
+    hours, minutes, seconds = (["0", "0"] + s.split(":"))[-3:]
+    hours = int(hours)
+    minutes = int(minutes)
+    seconds = float(seconds)
+    miliseconds = int(3600000 * hours + 60000 * minutes + 1000 * seconds)
+    return miliseconds
 
 # The App itself
 
@@ -71,13 +81,6 @@ app.layout = html.Div([
             html.Br(),
             dcc.Graph(
                 id='world-map-cricuits'
-        )], className='box', style={'margin-top': '3%'}),
-
-        html.Div([
-            html.Label('Circuit Choice'),
-            dcc.Dropdown(
-            id='circuit_drop',
-            options=[]
         )], className='box', style={'margin-top': '3%'})
 
     ],style={'margin-left': '3%'}),
@@ -101,11 +104,17 @@ app.layout = html.Div([
                 html.H3(id='circ_msc')
             ],className='box_circ_info'),
         html.Div([
-                html.H4('Number of DNF', style={'font-weight':'normal'}),
-                html.H3(id='number_dnf')
+                html.H4('Number of Accidents', style={'font-weight':'normal'}),
+                html.H3(id='number_accidents')
             ],className='box_circ_info')
-    ], className='box', style={'margin-top': '3%', 'margin-left': '5%'}),   
-
+    ], className='box', style={'margin-top': '3%', 'margin-left': '5%'}), 
+    
+    html.Div([
+        html.H2('Fastest Lap Time Evoltuion:'),
+        html.Br(),
+        dcc.Graph(
+            id='lap-time'
+    )], className='box', style={'margin-top': '3%', 'margin-left': '20%'}),   
 
     html.Div([
         html.H2('Winning Drivers:'),
@@ -145,8 +154,7 @@ app.layout = html.Div([
 ################################CALLBACK############################################
 
 @app.callback(
-    [Output(component_id='world-map-cricuits', component_property='figure'),
-     Output('circuit_drop', 'options')],
+    Output(component_id='world-map-cricuits', component_property='figure'),
     [Input('season_slider', 'value')]
 )
 
@@ -185,33 +193,35 @@ def callback_1(year_value):
                                           landcolor='black',
                                           lakecolor='white'
                                          ),
+                                
+                                margin = dict(t = 0, b = 0, l = 0, r=0),
 
-                                 title=dict(text='Circuits Around the World',
+                                title=dict(text='Circuits Around the World',
                                             x=.5 # Title relative position according to the xaxis, range (0,1)
                                            )
                                 )
-    return go.Figure(data=data_scattergeo, layout=layout_scattergeo), \
-           [{'label': c, 'value': c} for c in sorted(df_seasons['circuits.name'].unique())]
-              
+    return go.Figure(data=data_scattergeo, layout=layout_scattergeo)
 
 ################################CALLBACKFUNCTIONCIRCUITS2############################
 @app.callback(
-    [Output(component_id='winning-drivers', component_property='figure'), 
+    [Output(component_id='lap-time', component_property='figure'),
+     Output(component_id='winning-drivers', component_property='figure'), 
      Output(component_id='winning-constructors', component_property='figure'), 
      Output(component_id='circ_city', component_property='children'), # circ _info call backs start here
      Output(component_id='circ_country', component_property='children'),
      Output(component_id='circ_msd', component_property='children'),
      Output(component_id='circ_msc', component_property='children'),
-     Output(component_id='number_dnf', component_property='children')],
+     Output(component_id='number_accidents', component_property='children')],
     [Input(component_id="season_slider", component_property='value'),
-     Input(component_id="circuit_drop", component_property='value')]
+     Input(component_id="world-map-cricuits", component_property='clickData')]
 )
 
 ################################CALLBACK2FUNCTIONCIRCUITS############################
-def callback_2(year_value, circuit_value):   
+def callback_2(year_value, click_map): 
+    if click_map is None: 
+        return dash.no_update  
+    circuit_value = click_map['points'][0]['text']
     # year loc
-    if (circuit_value is None):
-        return dash.no_update
     if year_value[0] >= year_value[1]:
         year_value_max = year_value[0]
         year_value_min = year_value[1]
@@ -230,47 +240,50 @@ def callback_2(year_value, circuit_value):
     country = df_final_circuit['circuits.country'].unique()[0]
     msd = df_final_circuit[(df_final_circuit['circuits.name'] == str(circuit_value)) & (df_final_circuit['positionText'] == 1)].groupby('drivers.fullname')['drivers.fullname'].count().sort_values(ascending = False).index.tolist()[0]
     msc = df_final_circuit[(df_final_circuit['circuits.name'] == str(circuit_value)) & (df_final_circuit['positionText'] == 1)].groupby('constructors.name')['constructors.name'].count().sort_values(ascending = False).index.tolist()[0]    
-    number_ret = len(df_final_circuit[df_final_circuit['positionText'] == 'R'])
+    number_accidents = len(df_final_circuit[(df_final_circuit['statusId'] == 3) | (df_final_circuit['statusId'] == 4)])
+    #### LineChart Best Lap Times ####
+    gen_fast_lap = df_final_circuit[df_final_circuit['rank']==1]
+    gen_fast_lap['fastestLapTime'] = gen_fast_lap['fastestLapTime'].astype('str').apply(lambda x: time_to_mili(x))
+    laptime = px.scatter(gen_fast_lap, x="races.year", y="fastestLapTime", title='Fastest Lap Time',color_discrete_sequence=px.colors.sequential.RdBu, color="drivers.fullname", symbol="drivers.fullname")
+    laptime.update_layout(font_color = 'white', paper_bgcolor='rgba(0,0,0,0)', yaxis = {'type': 'date','tickformat': '%M:%S:%ss'},
+                      plot_bgcolor='rgba(0,0,0,0)')
 
     #### Boxplot for winning drivers
     #circuit_name = 'Bahrain International Circuit'
     #circuit_value = circuit_name
-    
-    print(circuit_value)
-    data_bar = dict(type='bar',
-                    y=df_seasons[(df_seasons['circuits.name'] == str(circuit_value)) & (df_seasons['positionText'] == 1)].groupby('drivers.surname')['drivers.surname'].count().sort_values(ascending = False).tolist(),
-                    x=df_seasons[(df_seasons['circuits.name'] == str(circuit_value)) & (df_seasons['positionText'] == 1)].groupby('drivers.surname')['drivers.surname'].count().sort_values(ascending = False).index.tolist(),
-                    texttemplate='<b>%{y} Wins</b>',
-                    textposition='outside'
-                    )
-
-    layout_bar = dict(yaxis=dict(range=(0, 10),
-                                 title='Number of Wins'
-                                 )
-                      )
+    driver_points = df_final_circuit.groupby(['drivers.fullname'])['points'].sum().sort_values(ascending = True).nlargest(15).reset_index()
+    pointsplot = px.bar(driver_points, x='drivers.fullname', y = 'points', labels={'drivers.fullname':'Drivers Name', 'points': 'Points'}, color_discrete_sequence=px.colors.sequential.RdBu, text='points')
+    #fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)',
+    #                  marker_line_width=1.5, opacity=0.6)
+    pointsplot.update_traces(textposition='outside',  texttemplate='<b>%{y} Points</b>')
+    pointsplot.update_layout(font_color = 'white', paper_bgcolor='rgba(0,0,0,0)',
+                        plot_bgcolor='rgba(0,0,0,0)')
 
     #### Pie Chart 
-    import plotly.express as px
     contructors =df_final_circuit[(df_final_circuit['circuits.name'] == str(circuit_value)) & (df_final_circuit['positionText'] == 1)].groupby(['constructors.name', 'drivers.surname']).agg({'resultId': 'nunique'})
     contructors.reset_index(inplace = True)
-    fig = px.sunburst(contructors, path=['constructors.name', 'drivers.surname'], values='resultId',color_discrete_sequence=px.colors.sequential.RdBu)
-
-    return go.Figure(data=data_bar, layout=layout_bar), \
-           fig, str(city), str(country), str(msd), str(msc),str(number_ret)    
+    fig = px.sunburst(contructors, 
+                     path=['constructors.name', 'drivers.surname'],
+                     values='resultId',color_discrete_sequence=px.colors.sequential.RdBu)
+    fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',
+                      plot_bgcolor='rgba(0,0,0,0)')
+    return laptime, pointsplot, \
+           fig, str(city), str(country), str(msd), str(msc),str(number_accidents)    
 
 ################################CALLBACKFUNCTIONCIRCUITS3############################
 @app.callback(
     Output('dvsc_dropdown1', 'options'),
     [Input(component_id='season_slider', component_property='value'),
-     Input(component_id="circuit_drop", component_property='value'),
+     Input(component_id="world-map-cricuits", component_property='clickData'),
      Input(component_id='dvsc_radio', component_property='value')
      ]
 )
 ################################CALLBACK3FUNCTIONCIRCUITS############################
-def callback_3(year_value, circuit_value, dvsc_value):
+def callback_3(year_value, click_map, dvsc_value):
     # year loc
-    if (circuit_value is None):
-        return dash.no_update
+    if click_map is None: 
+        return dash.no_update  
+    circuit_value = click_map['points'][0]['text']
     if year_value[0] >= year_value[1]:
         year_value_max = year_value[0]
         year_value_min = year_value[1]
@@ -290,16 +303,17 @@ def callback_3(year_value, circuit_value, dvsc_value):
 @app.callback(
     Output(component_id='finishing_races', component_property='figure'),
     [Input(component_id='season_slider', component_property='value'),
-     Input(component_id="circuit_drop", component_property='value'),
+     Input(component_id="world-map-cricuits", component_property='clickData'),
      Input(component_id="dvsc_radio", component_property='value'),
      Input(component_id='dvsc_dropdown1', component_property='value')
      ]
 )
 ################################CALLBACK4FUNCTIONCIRCUITS############################
-def callback_4(year_value, circuit_value, dvsc_value, client_dc_value):
+def callback_4(year_value, click_map, dvsc_value, client_dc_value):
     # year loc
-    if (circuit_value is None):
-        return dash.no_update
+    if click_map is None: 
+        return dash.no_update  
+    circuit_value = click_map['points'][0]['text']
     if year_value[0] >= year_value[1]:
         year_value_max = year_value[0]
         year_value_min = year_value[1]
@@ -359,6 +373,8 @@ def callback_4(year_value, circuit_value, dvsc_value, client_dc_value):
         y=pplot_percentages[filter_dc],
         x=pplot_percentages['nraces_percentage'],
         name='No DNF',
+        text=pplot_percentages['nraces'],
+        textposition='outside',
         orientation='h',
         marker=dict(
             color='rgba(69, 69, 69, 0.6)',
